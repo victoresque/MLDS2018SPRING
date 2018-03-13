b32c08fde53803400e3bd8866f43d10a2883dd47
Victor Huang <vichuang1997@gmail.com>
2018-03-13 13:07:31 +0800

    Move template to dedicated public repo

--- template/models/loss.py
+++
@@ -1,5 +0,0 @@
-import torch.nn.functional as f
-
-
-def nll_loss(y_input, y_target):
-    return f.nll_loss(y_input, y_target)
--- template/utils/util.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-def ensure_dir(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
