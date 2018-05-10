09018a5d6510529cfe606dddcf81a8c43ffada7a
Jason <jh0935822331@gmail.com>
2018-05-11 00:39:42 +0800

    hw2-2 script fixed

--- hw2/hw2_2/hw2_seq2seq.sh
+++ hw2/hw2_2/hw2_seq2seq.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 mkdir -p saved/baseline/
 wget -O saved/baseline/checkpoint.tar 'https://www.dropbox.com/s/9c4ptbtwuruh0je/checkpoint.tar?dl=1'
-wget -O svaed/baseline/embedder.pkl 'https://www.dropbox.com/s/2ekvpgpmtn9x67z/embedder.pkl?dl=1'
-python caption_test.py --name baseline --checkpoint checkpoint.tar --data-dir $1 --output $2
+wget -O saved/baseline/embedder.pkl 'https://www.dropbox.com/s/2ekvpgpmtn9x67z/embedder.pkl?dl=1'
+python chatbot_test.py --name baseline --checkpoint checkpoint.tar --input $1 --output $2
