ab1d0816d6078b02e0745db3785ca8719f347fbb
Victor Huang <vichuang1997@gmail.com>
2018-05-11 23:22:35 +0800

    Update hw2_seq2seq.sh

--- hw2/hw2_1/hw2_seq2seq.sh
+++ hw2/hw2_1/hw2_seq2seq.sh
@@ -2,4 +2,4 @@
 mkdir -p saved/baseline/
 wget -O saved/baseline/checkpoint.tar 'https://www.dropbox.com/s/ogq4r9m3ufw2zse/checkpoint.tar?dl=1'
 wget -O saved/baseline/embedder.pkl 'https://www.dropbox.com/s/h5meab4u5z0zqz6/embedder.pkl?dl=1'
-python caption_test.py --name baseline --checkpoint checkpoint.tar --data-dir $1 --output $2
+python3 caption_test.py --name baseline --checkpoint checkpoint.tar --data-dir $1 --output $2
