3ad49bd5acb05e72b0b18f91a9219b2998cbf9e0
Victor Huang <vichuang1997@gmail.com>
2018-05-11 23:22:52 +0800

    Update hw2_seq2seq.sh

--- hw2/hw2_2/hw2_seq2seq.sh
+++ hw2/hw2_2/hw2_seq2seq.sh
@@ -2,4 +2,4 @@
 mkdir -p saved/baseline/
 wget -O saved/baseline/checkpoint.tar 'https://www.dropbox.com/s/9c4ptbtwuruh0je/checkpoint.tar?dl=1'
 wget -O saved/baseline/embedder.pkl 'https://www.dropbox.com/s/2ekvpgpmtn9x67z/embedder.pkl?dl=1'
-python chatbot_test.py --name baseline --checkpoint checkpoint.tar --input $1 --output $2
+python3 chatbot_test.py --name baseline --checkpoint checkpoint.tar --input $1 --output $2
