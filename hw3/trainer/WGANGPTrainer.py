d90fb266bc172de3cd98d6d30d5224b6778ee2da
Victor Huang <vichuang1997@gmail.com>
2018-05-23 21:33:26 +0800

    HW3 WGAN-GP default configurations, trainer fixed

--- hw3/trainer/WGANGPTrainer.py
+++ hw3/trainer/WGANGPTrainer.py
@@ -65,16 +65,16 @@ class WGANGPTrainer(BaseTrainer):
             input_noise = torch.randn(self.batch_size, self.noise_dim, 1, 1)
             real_images = np.transpose(real_images, (0, 3, 1, 2))  # (batch, channel(BGR), width, height)
 
-            real_labels = torch.ones(self.batch_size)
-            fake_labels = -torch.ones(self.batch_size)
+            real_target = -torch.ones(self.batch_size)
+            fake_target = torch.ones(self.batch_size)
 
             # add noise to real images (tip 13)
             image_noise = torch.randn(*real_images.shape) * self.image_noise_var
             image_noise = self.image_noise_decay(image_noise, epoch)
             real_images = real_images + image_noise
 
-            input_noise, real_labels, fake_labels, real_images = \
-                to_variable(self.with_cuda, input_noise, real_labels, fake_labels, real_images)
+            input_noise, real_target, fake_target, real_images = \
+                to_variable(self.with_cuda, input_noise, real_target, fake_target, real_images)
 
             # training on discriminator
             fake_images = self.model.generator(input_noise)
@@ -84,8 +84,8 @@ class WGANGPTrainer(BaseTrainer):
             real_critic = self.model.discriminator(real_images)
             fake_critic = self.model.discriminator(fake_images)
             gp_critic = self.model.discriminator(interpolated_images)
-            real_loss = wasserstein_loss(real_critic, real_labels)
-            fake_loss = wasserstein_loss(fake_critic, fake_labels)
+            real_loss = wasserstein_loss(real_critic, real_target)
+            fake_loss = wasserstein_loss(fake_critic, fake_target)
             gp_loss = gradient_penalty(gp_critic, interpolated_images)
 
             self.dis_optimizer.zero_grad()
@@ -104,7 +104,7 @@ class WGANGPTrainer(BaseTrainer):
             if batch_idx % self.dis_iter == 0:
                 for i in range(self.gen_iter):
                     input_noise = torch.randn(*input_noise.size())
-                    fake_target = torch.ones(self.batch_size)
+                    fake_target = -torch.ones(self.batch_size)
                     input_noise, fake_target = to_variable(self.with_cuda, input_noise, fake_target)
                     fake_images = self.model.generator(input_noise)
                     fake_critic = self.model.discriminator(fake_images)
