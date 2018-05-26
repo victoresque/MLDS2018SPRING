977bdc5204e1e2a044509c8d7f48206ec89d2785
Jason <jh0935822331@gmail.com>
2018-05-27 04:27:29 +0800

    hw3-2 dataloader init

--- hw3/data_loader/data_loaders.py
+++ hw3/data_loader/data_loaders.py
@@ -1,7 +1,8 @@
-import torch, cv2, os
+import torch, cv2, os, pickle
 import numpy as np
 from torchvision import datasets, transforms
 from base import BaseDataLoader
+from utils import Embedder, ensure_dir
 
 
 class GanDataLoader(BaseDataLoader):
@@ -27,6 +28,121 @@ class GanDataLoader(BaseDataLoader):
             if img.shape != (64, 64, 3):
                 img = cv2.resize(img, (64, 64))
             self.images.append(img)
+        self.images = (np.array(self.images) - 127.5)/127.5
+
+    def __iter__(self):
+        super(GanDataLoader, self).__iter__()
+        return self
+
+    def __next__(self):
+        """
+        :return: img
+            shape = (batch_size, 64, 64, 3)
+        """
+        batch = super(GanDataLoader, self).__next__()
+        batch = [np.array(sample) for sample in batch]
+        return batch
+
+    def _pack_data(self):
+        return list(zip(self.images))
+
+    def _unpack_data(self, packed):
+        unpacked = list(zip(*packed))
+        unpacked = [list(item) for item in unpacked]
+        return unpacked
+
+    def _update_data(self, unpacked):
+        self.images, = np.array(unpacked)
+
+    def _n_samples(self):
+        return len(self.images)
+
+
+class CGanDataLoader(BaseDataLoader):
+    def __init__(self, config):
+        super(GanDataLoader, self).__init__(config)
+        self.data_dir1 = config['data_loader']['data_dir1']
+        self.data_dir2 = config['data_loader']['data_dir2']
+        self.tags1_path = os.path.join(self.data_dir1, '../tags_clean.csv')
+        self.tags2_path = os.path.join(self.data_dir2, '../tags.csv')
+        self.img_tags1, self.img_tags2 = {}, {}  # format: {
+                                                 #           img_idx0: {'hair': ['red'], 'eyes': ['blue']}
+                                                 #           img_idx1: {'hair': ['blonde'], 'eyes': ['yellow']}
+                                                 #           ...
+                                                 #         }
+        self.embedder = None
+        self.images = []
+        self.encoded_tags = []
+        self.__parse_features(os.path.join(config['trainer']['save_dir'],
+                                           config['name'], 'embedder.pkl'))
+        self.__parse_images()
+
+    def __parse_features(self, embedder_path):
+        hair_attr, eyes_attr = [], []
+        hair_stoplist = ['damage', 'long', 'short', 'pubic']
+        eyes_stoplist = ['bicolored', 'gray']
+        with open(self.tags1_path) as f:
+            for line in f:
+                idx, tags = line.split(',')
+                tags = tags.split('\t')
+                idx = int(idx)
+                self.img_tags1[idx] = {'hair': [], 'eyes': []}
+                for i, t in enumerate(tags):
+                    t = t.strip(':0123456789')
+                    t = t.split()
+                    if 'hair' in t:
+                        if t[0] not in hair_stoplist:
+                            self.img_tags1[idx]['hair'].append(t[0])
+                            if t[0] not in hair_attr:
+                                hair_attr.append(t[0])
+
+                    if 'eyes' in t and t[0] != 'eyes':
+                        if t[0] not in eyes_stoplist:
+                            self.img_tags1[idx]['eyes'].append(t[0])
+                            if t[0] not in eyes_attr:
+                                eyes_attr.append(t[0])
+
+                if len(self.img_tags1[idx]['hair']) != 1 or len(self.img_tags1[idx]['eyes']) != 1:
+                    self.img_tags1.pop(idx)
+                    
+        with open(self.tags2_path) as f:
+            for line in f:
+                idx, tags = line.split(',')
+                tags = tags.split()
+                idx = int(idx)
+                if tags[0] not in hair_attr:
+                    hair_attr.append(tags[0])
+                if tags[2] not in eyes_attr:
+                    eyes_attr.append(tags[2])
+                self.img_tags2[idx] = {'hair': [tags[0]], 'eyes': [tags[2]]}
+        
+        hair_attr.sort()
+        eyes_attr.sort()
+        self.embedder = Embedder(hair_attr, eyes_attr)
+        ensure_dir(os.path.dirname(embedder_path))
+        pickle.dump(self.embedder, open(embedder_path, 'wb'))
+
+    def __parse_images(self):
+        # add images in data_dir1
+        for img_file in sorted(os.listdir(self.data_dir1), key=lambda x: int(x[:x.rfind('.')])):
+            img_idx = int(img_file[:img_file.rfind('.')])
+            if img_idx not in self.img_tags1.keys(): continue
+            img_path = os.path.join(self.data_dir1, img_file)
+            img = cv2.imread(img_path)
+            if img.shape != (64, 64, 3):
+                img = cv2.resize(img, (64, 64))
+            self.images.append(img)
+            self.encoded_tags.append(self.embedder.encode_feature(self.img_tags1[img_idx]))
+        # add images in data_dir2
+        for img_file in sorted(os.listdir(self.data_dir2), key=lambda x: int(x[:x.rfind('.')])):
+            img_idx = int(img_file[:img_file.rfind('.')])
+            if img_idx not in self.img_tags2.keys(): continue
+            img_path = os.path.join(self.data_dir2, img_file)
+            img = cv2.imread(img_path)
+            if img.shape != (64, 64, 3):
+                img = cv2.resize(img, (64, 64))
+            self.images.append(img)
+            self.encoded_tags.append(self.embedder.encode_feature(self.img_tags2[img_idx]))
         self.images = (np.array(self.images) - 127.5)/127.5
 
     def __iter__(self):
--- hw3/utils/util.py
+++ hw3/utils/util.py
@@ -54,3 +54,30 @@ def print_status(epoch, batch_idx, n_trained, n_data, loss_d, loss_g):
     sys.stdout.flush()
     if batch_idx == n_data-1:
         print('')
+        
+class Embedder:
+    def __init__(self, hair_attr, eyes_attr):
+        self.hair_attr = hair_attr
+        self.eyes_attr = eyes_attr
+        self.hair_dict = {}
+        self.eyes_dict = {}
+        for i, attr in enumerate(self.hair_attr):
+            encode_attr = np.zeros(len(self.hair_attr))
+            encode_attr[i] = 1
+            self.hair_dict[attr] = encode_attr
+        for i, attr in enumerate(self.eyes_attr):
+            encode_attr = np.zeros(len(self.eyes_attr))
+            encode_attr[i] = 1
+            self.eyes_dict[attr] = encode_attr
+            
+    def encode_feature(self, feature):
+        """
+            feature : {'hair': ['color1'], 'eyes': ['color2']}
+            :return: np.array shape = (12 + 10,)
+                                      hair code first, eyes code follows 
+        """
+        hair_code = self.hair_dict[feature['hair'][0]]
+        eyes_code = self.eyes_dict[feature['eyes'][0]]
+        return np.concatenate((hair_code, eyes_code), dim=0)
+            
+        
