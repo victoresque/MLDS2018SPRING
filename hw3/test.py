f0753be566777439065baeb5d5bea243c72c668d
Victor Huang <vichuang1997@gmail.com>
2018-06-06 03:07:12 +0800

    HW3 testing script fix (img -> (img+1)/2)

--- hw3/test.py
+++ hw3/test.py
@@ -31,13 +31,13 @@ def main(args):
         input_noise = torch.randn(1, config['model']['noise_dim'], 1, 1)
         input_cond = embedder.encode_feature(cond)
         input_cond = np.expand_dims(input_cond, 0)
-        cond_scale = np.random.uniform(0.5, 0.7, (input_cond.shape[0], 1))
+        cond_scale = np.random.uniform(0.8, 1.0, (input_cond.shape[0], 1))
         cond_bias = np.array([np.random.uniform(0, 1 - i[0]) for i in cond_scale]).reshape(cond_scale.shape)
         input_cond = input_cond * cond_scale + cond_bias
         input_noise, input_cond = to_variable(with_cuda, input_noise, input_cond)
         gen_image = model.generator(input_noise, input_cond).cpu().data.numpy()[0]
         gen_image = np.transpose(gen_image, (1, 2, 0))
-        cv2.imshow('', gen_image)
+        cv2.imshow('', (gen_image+1)/2)
         cv2.waitKey()
 
 
