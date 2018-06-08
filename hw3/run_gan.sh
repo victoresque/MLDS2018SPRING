2b1e45644164440f1a73b065a0027a984e2b901e
Victor Huang <vichuang1997@gmail.com>
2018-06-08 19:07:20 +0800

    HW3 shell script fix

--- hw3/run_gan.sh
+++ hw3/run_gan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 mkdir -p saved/baseline_gan/
-# wget -O saved/baseline_gan/checkpoint.tar 'https://www.dropbox.com/s/mr4y85lucehzvzc/checkpoint-epoch100-loss-2.7593.pth.tar?dl=1'
+wget -O saved/baseline_gan/checkpoint.tar 'https://www.dropbox.com/s/mr4y85lucehzvzc/checkpoint-epoch100-loss-2.7593.pth.tar?dl=1'
 python3 test_gan.py --name baseline_gan --checkpoint checkpoint.tar --output samples/gan.png
